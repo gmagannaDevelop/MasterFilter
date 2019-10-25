e94f78b3a6890416fe6b05a581be10b65cac0d8c
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-10-25 00:17:54 -0500

    :zap: (Filtro_maestro.*): Finished with the notebook
    Modifying README.md on next commits.

--- Filtro_maestro.py
+++ Filtro_maestro.py
@@ -35,279 +35,133 @@ from PIL import Image
 import scipy.io as io
 
 
-# In[2]:
+# In[19]:
 
 
-def reimport(module_name: str, alias: str = None, partial: bool = False) -> NoReturn:
-    """
-        Useless piece of code, literally.
-    """
-    _exec_str: str = ''
-    
-    if alias and not partial:
-        _exec_str  = f"import {module_name} as {alias}\n"
-        _exec_str += f"importlib.reload({alias})\n"
-        _exec_str += f"from {module_name} import * \n"
-    elif alias and partial:
-        _exec_str  = f"import {module_name} as {alias}\n"
-        _exec_str += f"importlib.reload({alias})\n"
-    elif not alias and not partial:
-        _exec_str  = f"import {module_name} as __reimport_tmp\n"
-        _exec_str += f"importlib.reload(__reimport_tmp)\n"
-        _exec_str += f"from {module_name} import * \n"
-        
-    exec(_exec_str)
-
-##
-
-
-# In[119]:
-
-
-# Importamos todas nuestras funciones (le Gus):
+# Importamos todas nuestras funciones:
 import mfilt_funcs as mine
 importlib.reload(mine)
 from mfilt_funcs import *
 
 
+# In[3]:
+
+
+plt.rcParams['figure.figsize'] = (5, 5)
+
+
 # In[4]:
-
-
-# Importamos todas nuestras funciones (la Pats):
-"""
-import filtro_notch as lapats
-importlib.reload(lapats)
-from filtro_notch import *
-"""
-
-
-# In[5]:
-
-
-def black_and_white(input_image_path):
-   return Image.open(input_image_path).convert('L')
-
-
-# In[6]:
-
-
-plt.rcParams['figure.figsize'] = (10, 10)
-
-
-# In[7]:
 
 
 eps = np.finfo(float).eps
 eps.setflags(write=False)
 
 
-# In[8]:
+# In[5]:
 
 
 I = img.imread('imagenes/mama.tif')
 plt.imshow(I, cmap='gray')
 
 
-# In[9]:
+# In[6]:
 
 
 fft_viz(I)
 
 
-# In[10]:
+# In[7]:
 
 
 img_surf(I)
 
 
-# In[11]:
-
-
-HighI = kernel_highpass(pre_fft_processing(I), Do=1500, form='gauss')
-
-
-# In[12]:
-
-
-img_surf(HighI)
-
-
-# In[13]:
-
-
-plt.imshow(HighI, cmap='gray')
-
-
-# In[14]:
-
-
-list(map(cv2.getOptimalDFTSize, I.shape))
-
-
-# In[15]:
-
-
-I.shape
-
-
-# In[16]:
-
-
-newI = pre_fft_processing(I)
-
-
-# In[41]:
+# In[8]:
 
 
 x = cv2.imread('imagenes/RadiografiaRuidoCoherente.jpg', 0)
 
 
-# In[42]:
-
-
-#io.m
-
-
-# In[43]:
+# In[9]:
 
 
 plt.imshow(x, cmap='gray')
 
 
-# In[44]:
+# In[10]:
 
 
 fft_viz(x)
 
 
-# In[21]:
+# In[11]:
 
 
-"""
-    Ideas = crear una matriz de desplazamientos.
-    
-"""
+img_surf(x)
 
+
+# ### 5.1 Filtro pasa bajos ideal con wc=64,
+
+# In[12]:
+
+
+plt.imshow(
+    filtra_maestra(I, Do=64, kind='lowpass', form='ideal'),
+    cmap='gray'
+)
+
+
+# ### 5.2 Filtro pasa bajos butt con wc=64, orden=2
+
+# In[13]:
+
+
+plt.imshow(
+    filtra_maestra(I, Do=64, kind='lowpass', form='btw', n=2),
+    cmap='gray'
+)
+
+
+# ### 5.3 Filtro pasa bajos gauss con wc=64
+
+# In[15]:
+
+
+plt.imshow(
+    filtra_maestra(I, Do=64, kind='lowpass', form='gauss'),
+    cmap='gray'
+)
+
+
+# ### 5.4 Filtro pasa altos gauss con wc=64
+
+# In[17]:
+
+
+plt.imshow(
+    filtra_maestra(I, Do=64, kind='highpass', form='gauss'),
+    cmap='gray'
+)
+
+
+# ### 5.5 Filtro pasa bandas gauss con wc1=54, wc2=74
+
+# In[20]:
+
+
+plt.imshow(
+    filtra_maestra(I, wc1=54, wc2=74, kind='bandpass', form='gauss'),
+    cmap='gray'
+)
+
+
+# ### 5.6 Filtro rechazo de bandas gauss con wc1=54, wc2=74
 
 # In[22]:
 
 
-img_surf(distance_meshgrid_2D(I))
-
-
-# In[23]:
-
-
-plt.imshow(I, cmap='gray')
-
-
-# In[24]:
-
-
-plt.imshow(kernel_highpass(I, Do=500, form='ideal'), cmap='gray')
-
-
-# In[25]:
-
-
-img_surf(kernel_lowpass(I, form='btw', Do=640, n=9), colormap=cm.viridis)
-
-
-# In[ ]:
-
-
-
-
-
-# In[40]:
-
-
-#dir(cm)
-
-
-# In[28]:
-
-
-type(cm.coolwarm)
-
-
-# In[29]:
-
-
-aiuto = aiuda = jelp = help
-
-
-# In[30]:
-
-
-aiuda(kernel_ideal)
-
-
-# In[32]:
-
-
-#kernel_ideal(I.shape[0], I.shape[1], 0, (100, 400), 0, 100)
-
-
-# In[33]:
-
-
-aiuto(master_kernel)
-
-
-# In[34]:
-
-
-reimport('mfilt_funcs')
-
-
-# In[35]:
-
-
-plt.imshow(master_kernel(I, Do=100, kind='high', form='gauss'), cmap='gray')
-
-
-# In[36]:
-
-
-fft_viz(x)
-
-
-# In[101]:
-
-
-img_surf(kernel_notch(I, d0=15, centro=(100, 60), tipo=1, n=4))
-
-
-# In[77]:
-
-
-type(my_kern)
-
-
-# In[73]:
-
-
-np.float64(my_kern)
-
-
-# In[74]:
-
-
-my_kern.
-
-
-# In[118]:
-
-
-plt.imshow(master_kernel(I, Do=10, center=(14, 55), kind='notchpass', form='btw'), cmap='gray')
-
-
-# In[120]:
-
-
 plt.imshow(
-    filtra_maestra(I, Do=64),
+    filtra_maestra(I, wc1=54, wc2=74, kind='bandreject', form='gauss'),
     cmap='gray'
 )
 
